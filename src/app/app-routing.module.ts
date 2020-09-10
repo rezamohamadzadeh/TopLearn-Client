660d817b371f8a1f103f0bd2dc2279bb876167af
Administrator <Administrator@DESKTOP-6IC4J3S>
2020-09-10 20:32:37 +0430

    Implement from zero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BodyComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: '**', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [				
-    AppComponent,
-      HeaderComponent,
-      MenuComponent,
-      BodyComponent,
-      FooterComponent
+  declarations: [							
+    AppComponent
    ],
   imports: [
     BrowserModule,
