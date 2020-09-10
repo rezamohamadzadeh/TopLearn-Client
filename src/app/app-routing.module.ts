783254feff3b3a989d530243c0fed7e05ad79aea
Administrator <Administrator@DESKTOP-6IC4J3S>
2020-09-10 20:33:39 +0430

    Fix buges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DetailsComponent } from './details/details.component';
-import { AppComponent } from './app.component';
-import { BodyComponent } from './body/body.component';
 
-const routes: Routes = [
-  { path: '', component: BodyComponent },
-  { path: 'details', component: DetailsComponent },
-  { path: '**', component: DetailsComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
